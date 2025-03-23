10fc7a5b86eff77f33d983de148053b34f698212
MuttiaSelgas <selgasmuttia@gmail.com>
2025-03-23 08:21:05 +0800

    first commit

---
+++ NOTEably-IT342/src/main/java/com/oauth/NOTEably_IT342/NotEablyIt342Application.java
@@ -0,0 +1,13 @@
+package com.oauth.NOTEably_IT342;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotEablyIt342Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotEablyIt342Application.class, args);
+	}
+
+}
---
+++ NOTEably-IT342/src/test/java/com/oauth/NOTEably_IT342/NotEablyIt342ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.NOTEably_IT342;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotEablyIt342ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
